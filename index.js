de01bf35b8cf2f5688a6edc054e42ff2a444cf96
tutyamxx <tutygoo@gmail.com>
2020-09-08 21:06:36 +0100

    Fixed CI/CD

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  is-discord-invitation - Checks if a string is an official Discord invitation link
- *  @version: v1.0.2
+ *  @version: v1.0.3
  *  @link: https://github.com/tutyamxx/is-discord-invitation
  *  @license: MIT
  **/
