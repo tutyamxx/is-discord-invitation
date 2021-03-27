20fbdf9fbc7116435b52e41be024d2e6aa878d6e
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2021-03-27 17:40:37 +0000

    Changed the version to automatically publish to npm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  is-discord-invitation - Checks if a string is an official Discord invitation link
- *  @version: v1.0.3
+ *  @version: v1.0.4
  *  @link: https://github.com/tutyamxx/is-discord-invitation
  *  @license: MIT
  **/
