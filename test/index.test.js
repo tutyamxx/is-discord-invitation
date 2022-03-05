711e2c5379e476d5b66977b65783703228c7bcc2
Alexandru Maxim <15928925+tutyamxx@users.noreply.github.com>
2022-03-05 16:33:22 +0000

    Added more discord supported url's

--- test/index.test.js
+++ test/index.test.js
@@ -1,13 +1,27 @@
 const isDiscordInvitation = require("../index.js");
 
 const validDiscordInviteUrl = [
+    // --| Official Discord URL
     "https://discord.gg/d8ZKqT",
     "https://discord.gg/MTgFwr",
     "https://discord.gg/BEXg3z",
     "https://discord.com/invite/qS2Sxgy",
     "https://discord.com/invite/ASGu7aC",
     "https://discordapp.com/invite/ASGu7aC",
-    "www.discordapp.com/invite/ASGu7aC"
+    "www.discordapp.com/invite/ASGu7aC",
+
+    // --| watchanimeattheoffice.com - Official Discord URL
+    "www.watchanimeattheoffice.com/invite/x34ad",
+    "https://watchanimeattheoffice.com/invite/1x2f3g",
+
+    // --| bigbean.solutions - Official Discord URL
+    "www.bigbean.solutions/invite/xsnhshbh3",
+    "https://bigbean.solutions/invite/12xf",
+    "bigbean.solutions/invite/43vx62v",
+
+    // --| dis.gd - Official Discord URL
+    "www.dis.gd/invite/qS2Sxgy",
+    "https://dis.gd/invite/MTgFwr"
 ];
 
 const randomDiscordUrl = [
@@ -23,14 +37,5 @@ const randomDiscordUrl = [
     "https://www.npmjs.com/package/is-discord-invite"
 ]
 
-test("Check for valid Discord invites", () => {
-    validDiscordInviteUrl.forEach((url) => {
-        expect(isDiscordInvitation(url)).toBe(true);
-    });
-});
-
-test("Check for random invalid Discord invite links", () => {
-    randomDiscordUrl.forEach((url) => {
-        expect(isDiscordInvitation(url)).toBe(false);
-    });
-});
+test("Check for valid Discord invites", () => validDiscordInviteUrl.forEach((url) => expect(isDiscordInvitation(url)).toBe(true)));
+test("Check for random invalid Discord invite links", () => randomDiscordUrl.forEach((url) => expect(isDiscordInvitation(url)).toBe(false)));
