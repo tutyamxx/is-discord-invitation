a09fe3db18e4652f1614bc5ad9f894c0d29899c9
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2023-10-20 18:06:50 +0100

    UPDATE: Updated version and dependencies

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 /**
  *  is-discord-invitation - Checks if a string is an official Discord invitation link
- *  @version: v1.0.9
+ *  @version: v1.0.10
  *  @link: https://github.com/tutyamxx/is-discord-invitation
  *  @license: MIT
  **/
 
 module.exports = (paramString) => {
-    if (!paramString || typeof paramString !== 'string') return false;
-    if (/(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite|discord.com\/invite|watchanimeattheoffice.com\/invite|dis.gd\/invite|bigbean.solutions\/invite)\/+[a-zA-Z0-9]{4,16}/gi.test(paramString)) return true;
+    if (!paramString || typeof paramString !== 'string') {
+        return false;
+    }
+
+    if (/(https?:\/\/)?(www\.)?(discord\.(gg|io|me|li)|discordapp\.com\/invite|discord.com\/invite|watchanimeattheoffice.com\/invite|dis.gd\/invite|bigbean.solutions\/invite)\/+[a-zA-Z0-9]{4,16}/gi.test(paramString)) {
+        return true;
+    }
 
     return false;
 };
