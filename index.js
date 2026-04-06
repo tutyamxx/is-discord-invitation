ada0f16d3097bbc10af21ac8b3051419eaf5f184
tutyamxx <15928925+tutyamxx@users.noreply.github.com>
2026-04-06 20:26:35 +0100

    UPDATE: Updated workflows

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
  *  is-discord-invitation - Checks if a string is an official Discord invitation link
- *  @version: v1.0.20
+ *  @version: v1.0.21
  *  @link: https://github.com/tutyamxx/is-discord-invitation
  *  @license: MIT
  **/
